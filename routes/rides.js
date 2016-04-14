89b7b55ae7c669e46a007bc9c629b9cdd0e82457
april sikes <aprilsikes@gmail.com>
2016-04-14 14:03:37 -0600

    css changes

--- routes/rides.js
+++ routes/rides.js
@@ -4,9 +4,33 @@ var knex = require('../db/knex');
 // var authChecker = require('./authChecker');
 // var jwt = require('jsonwebtoken');
 
+function Orgs() {
+  return knex('orgs');
+}
+
+function Groups() {
+  return knex('groups');
+}
+
+function Events() {
+  return knex('events');
+}
+
 function Rides() {
   return knex('rides');
 }
+
+router.get('/:orgs_id/groups/:groups_id/events/:events_id/rides/:id', function (req, res, next) {
+  Orgs().where('id', req.params.orgs_id).first().then(function (org) {
+    Groups().where('id', req.params.groups_id).then(function (group) {
+      Events().where('id', req.params.id).then(function (event) {
+        Rides().where('events_id', req.params.id).then(function (rides) {
+          res.json({org: org, group: group, event: event, rides: rides});
+        })
+      })
+    })
+  })
+})
 
 router.post('/:orgs_id/groups/:groups_id/events/:id/rides', function (req, res, next) {
   var ride = {};
--- routes/seats.js
+++ routes/seats.js
@@ -4,9 +4,33 @@ var knex = require('../db/knex');
 // var authChecker = require('./authChecker');
 // var jwt = require('jsonwebtoken');
 
+function Orgs() {
+  return knex('orgs');
+}
+
+function Groups() {
+  return knex('groups');
+}
+
+function Events() {
+  return knex('events');
+}
+
 function Seats() {
   return knex('seats');
 }
+
+router.get('/:orgs_id/groups/:groups_id/events/:events_id/seats/:id', function (req, res, next) {
+  Orgs().where('id', req.params.orgs_id).first().then(function (org) {
+    Groups().where('id', req.params.groups_id).then(function (group) {
+      Events().where('id', req.params.id).then(function (event) {
+        Seats().where('events_id', req.params.id).then(function (seats) {
+          res.json({org: org, group: group, event: event, seats: seats});
+        })
+      })
+    })
+  })
+})
 
 router.post('/:orgs_id/groups/:groups_id/events/:id/seats', function (req, res, next) {
   var seat = {};
